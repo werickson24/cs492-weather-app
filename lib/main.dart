207b982fe68ee5956f0c2b3e650f6a70386805e6
Wil Erickson <william@ericksonls.com>
2025-01-09 14:14:46 -0800

    did todos

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // TODO: Change this title to CS492 Weather App. This one will not appear in the emulator. Remove the TODO when complete
-      title: 'Flutter Demo',
+      title: 'CS492 Weather App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -32,8 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // TODO: Change this title to CS492 Weather App. This one will appear in the emulator. Remove the TODO when complete
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'CS492 Weather App'),
     );
   }
 }
